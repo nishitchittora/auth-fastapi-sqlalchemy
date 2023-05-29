9bec02925fb50443fd8cdc3a260ac9ebb14d485a
nishitchittora <nishitchittora2015@gmail.com>
2023-05-29 10:41:10 +0530

    Proj 3 todo updated

--- app/proj_3_todo/main.py
+++ app/proj_3_todo/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import Field
-from routers import auth, todos
+from routers import auth, todos, admin, users
 from database import engine
 from models import Base
 
@@ -10,3 +10,5 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
